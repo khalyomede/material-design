62b6f7298967d8e6c9c24b32ca6d062c66213dce
khalyomede <>
2019-02-16 20:59:53 +0100

    added new "chip" component

---
+++ src/components/chip.js
@@ -0,0 +1,21 @@
+"use strict";
+
+import css from "../sass/components/chip.sass";
+
+export class MaterialChip extends HTMLElement {
+	constructor() {
+		super();
+
+		const shadow = this.attachShadow({ mode: "open" });
+		const span = document.createElement("span");
+		const style = document.createElement("style");
+
+		span.innerHTML = this.innerHTML;
+		span.classList.add("chip");
+
+		style.textContent = css.toString();
+
+		shadow.appendChild(style);
+		shadow.appendChild(span);
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-'use strict';
+"use strict";
 
-import { MaterialInput } from './components/input';
+import { MaterialInput } from "./components/input";
+import { MaterialChip } from "./components/chip";
 
-customElements.define('material-input', MaterialInput);
+customElements.define("material-input", MaterialInput);
+customElements.define("material-chip", MaterialChip);
