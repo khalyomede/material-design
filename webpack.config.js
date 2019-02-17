c7e72aada1094d33db47dab77ad264f6fc5c9f40
Amin NAIRI <nairi.amin@gmail.com>
2019-02-17 09:18:45 +0100

    :wrench: added eslint configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,42 @@
-"use strict";
+'use strict';
 
-const { resolve } = require("path");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const UglifyjsWebpackPlugin = require("uglifyjs-webpack-plugin");
+const { resolve } = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyjsWebpackPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-	mode: "development",
-	entry: resolve("src", "index.js"),
-	target: "web",
+	mode: 'development',
+	entry: resolve('src', 'index.js'),
+	target: 'web',
 	output: {
-		filename: "material-design.js",
-		path: resolve("dist")
+		filename: 'material-design.js',
+		path: resolve('dist')
 	},
 	module: {
 		rules: [
 			{
+        test: /\.js$/,
+        enforce: 'pre',
+				exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true
+        }
+			},
+			{
 				test: /\.(scss|sass)$/,
 				exclude: /node_modules/,
-				use: ["css-loader", "sass-loader"]
+				use: ['css-loader', 'sass-loader']
 			},
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				loader: "babel-loader"
+				use: 'babel-loader'
 			}
 		]
 	},
 	optimization: {
 		minimizer: [new UglifyjsWebpackPlugin()]
 	},
-	plugins: [new CleanWebpackPlugin([resolve("dist")])]
+	plugins: [new CleanWebpackPlugin([resolve('dist')])]
 };
