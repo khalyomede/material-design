1ad22c11a91e5039939b1a9a2f97c58ad02f7067
khalyomede <>
2019-02-23 15:51:52 +0100

    improved sass build by compressing assets

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ const { src, dest, task, parallel } = require("gulp");
 const sass = require("gulp-sass");
 const rename = require("gulp-rename");
 const autoprefixer = require("gulp-autoprefixer");
+const cleanCss = require("gulp-clean-css");
 
 task("sass", () =>
 	src("src/index.sass")
 		.pipe(sass())
 		.pipe(autoprefixer())
+		.pipe(cleanCss())
 		.pipe(rename({ basename: "material-design" }))
 		.pipe(dest("dist"))
 );
